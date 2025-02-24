190556bb14c8b3d9aea037fb53293d1c686e4d63
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2025-02-24 11:31:10 -0500

    PRINTF_IF_I2C_ERROR

--- Error.h
+++ Error.h
@@ -8,3 +8,10 @@
 
 #define AVP_HAL_ASSERT_WITH_CODE(h_ptr,exp) do{ HAL_StatusTypeDef status = (exp); if(status != HAL_OK) \
     AVP_ERROR("Expression \"" #exp "\" failed with code %lu, status %u, in state %u!\n",(h_ptr)->ErrorCode, status, (h_ptr)->State); }while(0)
+
+#define PRINTF_IF_HAL_ERROR(h, exp, printf_fun) \
+HAL_StatusTypeDef s = (exp);\
+if(s != HAL_OK) {\
+printf_fun("HAL error in %s: %hu, code: %lu on line %d\n", \
+            __PRETTY_FUNCTION__, s, h.ErrorCode, __LINE__);
+
